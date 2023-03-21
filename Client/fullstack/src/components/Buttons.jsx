2b6010313cb900ccaa910675ef9e15ae545e4cd1
daniahih <daniahih310@gmail.com>
2023-03-21 13:21:55 +0200

    crud get , post , delete done

--- Client/fullstack/src/components/Buttons.jsx
+++ Client/fullstack/src/components/Buttons.jsx
@@ -1,19 +1,72 @@
+import { useState } from "react";
 export default function Buttons() {
+  const [productid, setProductid] = useState("");
+  const [productName, setProductName] = useState("");
+  const [productprice, setProductprice] = useState("");
   const handleClick = () => {
-    fetch("http://localhost:8181/api/my-endpoint", {
+    fetch("http://localhost:8181/products")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("res", res);
+      });
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setProductName(e.target.value);
+  };
+  const handleChangeid = (e) => {
+    setProductid(e.target.value);
+  };
+  const handleChangeprice = (e) => {
+    setProductprice(e.target.value);
+  };
+  const handelAdd = () => {
+    const product = { id: productid, name: productName, price: productprice };
+    console.log("client", product);
+    fetch("http://localhost:8181/products", {
       method: "POST",
-      body: JSON.stringify({ message: "Hello server!" }),
+      body: JSON.stringify(product),
       headers: {
         "Content-Type": "application/json",
       },
-    }).then((res) => {
-      console.log("res", res);
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("res", res);
+      });
+  };
+  const handdleDelete = async () => {
+    await fetch("http://localhost:8181/products", {
+      method: "DELETE",
     });
   };
+
   return (
     <>
       <h1> connect the react app to the server </h1>
-      <button onClick={handleClick}>Click me</button>
+      <button onClick={handleClick}>See Product</button>
+      <input
+        type="text"
+        value={productid}
+        onChange={handleChangeid}
+        placeholder="id"
+      ></input>
+      <input
+        type="text"
+        value={productName}
+        onChange={handleChange}
+        placeholder="name"
+      />
+      <input
+        type="text"
+        value={productprice}
+        onChange={handleChangeprice}
+        placeholder="price"
+      />
+
+      <button onClick={handelAdd}> add </button>
+      <button onClick={handdleDelete}>Delete</button>
     </>
   );
 }
--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 8181;
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+app.get("/products", (req, res) => {
+  const products = JSON.parse(fs.readFileSync("../database/product.json"));
+  res.send(products);
 });
-app.post("/api/my-endpoint", (req, res) => {
-  console.log("get record ");
-  const message = req.body.message;
-  console.log(message); // "Hello server!"
-  res.end("Thanks for your message!");
+app.post("/products", (req, res) => {
+  const { id, name, price } = req.body;
+  console.log(req.body);
+
+  const products = JSON.parse(fs.readFileSync("../database/product.json"));
+  products.push({ id: id, name: name, price: price });
+
+  fs.writeFileSync("../database/product.json", JSON.stringify(products));
+  res.send(products);
 });
+
+app.delete("/products", (req, res) => {
+  const products = JSON.parse(fs.readFileSync("../database/product.json"));
+  const deletedUser = products.shift();
+  fs.writeFileSync("../database/product.json", JSON.stringify(products));
+  res.send(deletedUser);
+});
+
+// app.post("/api/my-endpoint", (req, res) => {
+//   console.log("get record ");
+//   const message = req.body.message;
+//   console.log(message); // "Hello server!"
+//   res.end("Thanks for your message!");
+// });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
