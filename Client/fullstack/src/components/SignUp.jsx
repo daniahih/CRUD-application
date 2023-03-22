c3d2f640131910d778198586d3dbf8b95482931b
daniahih <daniahih310@gmail.com>
2023-03-22 13:48:03 +0200

    added Sign UP

--- Client/fullstack/src/components/SignUp.jsx
+++ Client/fullstack/src/components/SignUp.jsx
@@ -3,10 +3,30 @@ export default function SignUp() {
   function handleInput(type, e) {
     values[type] = e.target.value;
   }
+  function handleSubmit(e, type) {
+    e.preventDefault();
+    if (values.userName && values.password) {
+      const headers = new Headers();
+      headers.append("content-type", "application/json");
+      fetch(`http://localhost:8181/${type}`, {
+        headers,
+        method: "POST",
+        body: JSON.stringify(values),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log("res", res);
+        });
+    }
+  }
   return (
     <>
       <h1> SIGN UP </h1>
-      <form>
+      <form
+        onSubmit={(event) => {
+          handleSubmit(event, "signup");
+        }}
+      >
         <label> Username </label>
         <input
           type="text"
